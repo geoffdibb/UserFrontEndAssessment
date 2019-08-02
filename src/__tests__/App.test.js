1a16e556bb9000bc26c0d0dffcfa3b60bbbc8f40
geoffdibb <geoffdibb@yahoo.co.uk>
2019-08-02 14:43:37 +0100

    Altered tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
 
 it('snapshot App', () => {
   // const wrapper = shallow(<App />);
@@ -14,4 +14,10 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+it('renders Navbar', () => {
+  const wrapper = shallow(<App />);
+      const Navbar = <Navbar/>;
+  expect(wrapper.contains(Navbar)).toEqual(true);
+
 });
--- src/__tests__/Hometest.js
+++ src/__tests__/Hometest.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Home from '../Home';
-import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
-import App from '../App';
 
 
 it('snapshot Home', () => {
@@ -10,9 +8,3 @@ it('snapshot Home', () => {
   expect(tree).toMatchSnapshot();
 })
 
-it('renders Home text', () => {
-  const wrapper = shallow(<App />);
-      const Home = <Home/>;
-  expect(wrapper.contains(Home)).toEqual(true);
-
-});
